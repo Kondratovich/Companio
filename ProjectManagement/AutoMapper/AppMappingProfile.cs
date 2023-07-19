d3180f21d761322b6ffc0aa9705361a460e12b0b
Artsem Kandratovich <artsem.kondratovich@gmail.com>
2023-07-19 15:04:23 +0300

    add projectManagement microservice proj

---
+++ ProjectManagement/AutoMapper/AppMappingProfile.cs
@@ -0,0 +1,41 @@
+﻿using AutoMapper;
+using Companio.DTO;
+using Companio.Models;
+using MongoDB.Bson;
+using Task = Companio.Models.Task;
+
+namespace Companio.AutoMapper;
+
+public class AppMappingProfile : Profile
+{
+    public AppMappingProfile()
+    {
+        ConfigureFromDomainToDto();
+        ConfigureFromDtoToDomain();
+    }
+
+    private void ConfigureFromDomainToDto()
+    {
+        CreateMap<Project, ProjectReadDTO>();
+        CreateMap<Team, TeamReadDTO>();
+        CreateMap<Customer, CustomerReadDTO>();
+        CreateMap<User, UserReadDTO>();
+        CreateMap<Task, TaskReadDTO>();
+        CreateMap<AbsenceTimeline, AbsenceTimelineReadDTO>();
+        CreateMap<Absence, AbsenceDTO>();
+    }
+
+    private void ConfigureFromDtoToDomain()
+    {
+        CreateMap<ProjectDTO, Project>()
+            .ForMember(dest => dest.CustomerId, act => act.MapFrom(src => ObjectId.Parse(src.CustomerId)))
+            .ForMember(dest => dest.TeamId, act => act.MapFrom(src => ObjectId.Parse(src.TeamId)));
+        CreateMap<TeamDTO, Team>();
+        CreateMap<CustomerDTO, Customer>();
+        CreateMap<UserDTO, User>();
+        CreateMap<TaskDTO, Task>();
+        CreateMap<AbsenceTimelineDTO, AbsenceTimeline>()
+            .ForMember(dest => dest.UserId, act => act.MapFrom(src => ObjectId.Parse(src.UserId)));
+        CreateMap<AbsenceDTO, Absence>();
+    }
+}
---
+++ ProjectManagement/Controllers/ProjectsController.cs
@@ -0,0 +1,99 @@
+using AutoMapper;
+using Companio.Attributes;
+using Companio.DTO;
+using Companio.Models;
+using Companio.Models.Enums;
+using Companio.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+
+namespace Companio.Controllers;
+
+public class ProjectsController : Controller
+{
+    private readonly IMapper _mapper;
+    private readonly IProjectService _projectService;
+
+    public ProjectsController(IMapper mapper, IProjectService projectService)
+    {
+        _mapper = mapper;
+        _projectService = projectService;
+    }
+
+    [HttpGet("api/v1/projects")]
+    public ActionResult<List<ProjectReadDTO>> GetAll()
+    {
+        var projects = _projectService.GetAll();
+        var projectReadDtos = projects.Select(_mapper.Map<ProjectReadDTO>);
+        return Ok(projectReadDtos);
+    }
+
+    [HttpGet("api/v1/projects/{id}")]
+    public ActionResult<ProjectReadDTO> Get(string id)
+    {
+        if (!ObjectId.TryParse(id, out var objectId))
+            return ValidationProblem();
+
+        var project = _projectService.SingleByIdOrDefault(objectId);
+
+        if (project == null)
+            return NotFound();
+
+        var projectReadDto = _mapper.Map<ProjectReadDTO>(project);
+
+        return Ok(projectReadDto);
+    }
+
+    [HttpPost("api/v1/projects")]
+    [RolePermission(Role.Administrator, Role.Manager)]
+    public ActionResult<ProjectReadDTO> Create([FromBody] ProjectDTO projectDto)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var project = _mapper.Map<Project>(projectDto);
+        var createdProject = _projectService.Create(project);
+        var projectReadDto = _mapper.Map<ProjectReadDTO>(createdProject);
+
+        var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host.ToUriComponent()}";
+        var locationUri = baseUrl + $"/api/v1/projects/{project.Id}";
+
+        return Created(locationUri, projectReadDto);
+    }
+
+    [HttpPut("api/v1/projects/{id}")]
+    [RolePermission(Role.Administrator, Role.Manager)]
+    public ActionResult Put(string id, [FromBody] ProjectDTO projectDto)
+    {
+        if (!ObjectId.TryParse(id, out var objectId))
+            return ValidationProblem();
+
+        var project = _projectService.SingleByIdOrDefault(objectId);
+        if (project == null)
+            return NotFound();
+
+        _mapper.Map(projectDto, project);
+        _projectService.Update(project);
+        var outputDto = _mapper.Map<ProjectReadDTO>(project);
+
+        return Ok(outputDto);
+    }
+
+    [HttpDelete("api/v1/projects/{id}")]
+    [RolePermission(Role.Administrator, Role.Manager)]
+    public ActionResult Delete(string id)
+    {
+        if (!ObjectId.TryParse(id, out var objectId))
+            return ValidationProblem();
+
+        var project = _projectService.SingleByIdOrDefault(objectId);
+        if (project == null)
+            return NotFound();
+
+        _projectService.Delete(objectId);
+
+        return NoContent();
+    }
+}
